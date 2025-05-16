7ddaa580f832dc86fd63459717635c27d89f988c
shlokGupta <shlok.official01@gmail.com>
2025-05-17 03:18:37 +0530

    refactor: Remove unused Navbar and Footer imports from Cart page

--- src/app/(root)/(routes)/bubl-cart/page.tsx
+++ src/app/(root)/(routes)/bubl-cart/page.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-import Navbar from "@/app/components/navbar";
 // import HeroSection from "@/app/components/AboutUs/HeroSection";
 
-import Footer from "@/app/components/footer";
 import CartPage from "@/app/components/Cart/Cart";
 
 const Page = () => {
